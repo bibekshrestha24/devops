73d41423471c5d4f8d691fc625f307c5ba5c6f4e
Bibek Shrestha <bibek.shrestha23@gmail.com>
2021-01-13 20:11:59 -0600

    added http port 80 security group to the instance in uswest2

--- ec2_boto3.py
+++ ec2_boto3.py
@@ -1,9 +1,25 @@
 import boto3
 ec2 = boto3.resource('ec2')
+
+security_group = ec2.create_security_group(
+    Description='http',
+    GroupName='gigi'
+)
+security_group.authorize_ingress(
+    CidrIp='0.0.0.0/0',
+    IpProtocol='tcp',
+    FromPort=80,
+    ToPort=80
+)
+
+
 instance = ec2.create_instances(
     ImageId='ami-0a36eb8fadc976275',
     InstanceType='t2.micro',
     KeyName='bibek',
     MinCount=1,
-    MaxCount=1
-)    
+    MaxCount=1,
+    SecurityGroupIds=['gigi']
+)
+
+
